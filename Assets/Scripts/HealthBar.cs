3b12ca5e4e64285ad3b5c6fda5a8ddb33b49e0c6
VipGhost-dev <lied00@mail.ru>
2022-02-22 19:34:37 +0300

    Добавил количество патронов и полоску здоровья

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +18,20 @@ public class HealthBar : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    public void SetMaxHealth(float maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float currentHealth)
+    {
+        slider.value = currentHealth;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interface : MonoBehaviour
+{
+    public GameObject interfaceA;
+    public Text ammo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammo.text = $"{Player.pistolammomag} / {Player.inventorypistolammo}"; 
+    }
+}
